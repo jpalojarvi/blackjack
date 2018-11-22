bb7d1f1272b91f41f4540898579e381484a4305f
mkayyee <45204687+mkayyee@users.noreply.github.com>
2018-11-22 13:29:07 +0200

    kommentit lisätty kaikkiin riveihin

--- Game.java
+++ Game.java
@@ -17,35 +17,35 @@ public class Game extends Hand {
         // 2 ekaa korttii molemmille defaulttina ennen while looppia - lisätään niiden arvot luotuihin olioihin,
         // pelaajan kolmas kortti ja printit on vaan testaamisen takii
    
-        String pcard1 = Deck.deal();
-        String pcard2 = Deck.deal();
-        String pcard3 = Deck.deal();
+        String pcard1 = Deck.deal();      //Luodaan pelaajan ensimmäinen kortti
+        String pcard2 = Deck.deal();      //Luodaan pelaajan toinen kortti
+        String pcard3 = Deck.deal();      //Luodaan pelaajan kolmas kortti
 
-        String dcard1 = Deck.deal();
-        String dcard2 = Deck.deal();
+        String dcard1 = Deck.deal();    //Luodaan jakajan ensimmäinen kortti
+        String dcard2 = Deck.deal();    //Luodaan jakajan toinen kortti
         
-        Hand pHand = new Hand();
-        Hand dHand = new Hand();
+        Hand pHand = new Hand();    //Luodaan pelaajan käsi -olio
+        Hand dHand = new Hand();    //Luodaan jakajan käsi -olio
         
-        pHand.addHand(pcard1);
-        pHand.addHand(pcard2);
-        pHand.addHand(pcard3);
+        pHand.addHand(pcard1);      //Lisätään pelaajan ensimmäisen kortin arvo pelaajan käsi -olioon
+        pHand.addHand(pcard2);      //Lisätään pelaajan toisen kortin arvo pelaajan käsi -olioon
+        pHand.addHand(pcard3);      //Lisätään pelaajan kolmannen kortin arvo pelaajan käsi -olioon
         
-        pHand.addValue(pcard1);
-        pHand.addValue(pcard2);
-        pHand.addValue(pcard3);
+        pHand.addValue(pcard1);     //Lisätään pelaajan ensimmäisen kortin numeerinen arvo pelaajan käden olion total valueen
+        pHand.addValue(pcard2);     //Lisätään pelaajan toisen kortin numeerinen arvo pelaajan käden olion total valueen
+        pHand.addValue(pcard3);     //Lisätään pelaajan kolmannen kortin numeerinen arvo pelaajan käden olion total valueen
         
-        pHand.getHand();
-        System.out.println(pHand.GetTotal());
+        pHand.getHand();       //Tulostaa pelaajan koko käden stringeinä
+        System.out.println(pHand.GetTotal());   //tulostaa pelaajan korttien arvojen summan
         
-        dHand.addHand(dcard1);
-        dHand.addHand(dcard2);
+        dHand.addHand(dcard1);  //Lisätään jakajan ensimmäisen kortin arvo jakajan käden olioon
+        dHand.addHand(dcard2);  //Lisätään jakajan ensimmäisen kortin arvo pelaajan jakajan käden olioon
         
-        dHand.addValue(dcard1);
-        dHand.addValue(dcard2);
+        dHand.addValue(dcard1);//Lisätään jakajan ensimmäisen kortin numeerinen arvo pelaajan käden olion total valueen
+        dHand.addValue(dcard2);//Lisätään jakajan toisen kortin numeerinen arvo pelaajan käden olion total valueen
         
-        dHand.getHand();
-        System.out.println(dHand.GetTotal());
+        dHand.getHand(); //Tulostaa jakajan koko käden stringeinä
+        System.out.println(dHand.GetTotal()); //tulostaa jakajan korttien arvojen summan
         
         
     
