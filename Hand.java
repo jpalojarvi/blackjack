691e14cd0d0d52c3918bf116cac3bbcfdfbbf47d
jpalojarvi <34962671+jpalojarvi@users.noreply.github.com>
2018-11-28 12:49:36 +0200

    Update Hand.java

--- Hand.java
+++ Hand.java
@@ -1,47 +1,83 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Blackjack;
+package blackjack;
 
-/**
- *
- * @author Mkayyee
- */
-public class Hand extends Deck {
+
+public class Controller extends Game {
+    // kapseloidut ominaisuudet (V, M)
+    private View view;
+    // private Hand hand;
     
-    int value;
-    char cString;
-    private List<String> wholehand = new ArrayList();
-    int totalvalue;
+    Game aloitusnäyttö = new Game();
     
-    
-    public void getHand(){              // tulostaa koko käden
-        for (String hand : wholehand)    
-            System.out.print(hand+" "); 
-    }
-    public void addHand(String x){   // lisää kortin jo olemassa olevien joukkoon
-        wholehand.add(x)
-    }
-    public void pAddTotal(int add){     // ei tällä hetkellä mitään käyttöä, ehkä tarpeellinen
-        totalvalue = totalvalue + add;
-    }
-    public int GetTotal(){              // palauttaa käden arvon summan
-        return totalvalue;
-    }
-    public void addValue(String card){  // ottaa ekan merkin kortista ja muuntaa sen str muotoon, jos kuvakortti -> 
-                                        // lisää käden summaan 10, muuten parseroi suoraan intiksi ja lisää käden summaan            
-        cString = card.charAt(0);
-        String cardo = String.valueOf(cString);
+    public Controller() {
+        // Nyt kontrollerin (C) konstruktori (eli tämä metodi) 
+        // luo muut tarvittavat komponentit ja pyytää sitten 
+        // näyttöä esittämään aloitusnäytön.
         
-        if (cardo.equals("T") || cardo.equals("Q") || cardo.equals("J") || cardo.equals("K")){
-            totalvalue += 10;
+        // luodaan sovelluksen tarvitsemat muut komponentit: M, V.
+        /* playerhand = new Hand();            // M, alkusaldo 500.
+        dealerhand = new Hand();
+        cardpack = new Hand();
+        */
+        view = new View();                  // V
+        
+        // välitetään näytölle tieto sovelluksen kontrollerista eli
+        // tämä komponentti itse (this).
+        view.setController(this); 
+        
+        // pyydetään näyttöä esittämään aloitusnäyttö
+        view.startMenu();
+                        
+    }
+    
+    /*
+    // näytöltä tulevia pyyntöjä
+    public double balance() {
+        double cardBalance = cardpack.getBalance();
+        return cardBalance;
+    }
+    */
+    
+    /*public void take(double card) {
+        boolean successcheck;
+        successcheck = cardpack.takecard(cards);
+        // Ei kerrota nyt käyttäjälle toiminnon onnistumisesta mitään (???)
+        if (successcheck)
+        { 
+            view.showMessage("Nostosi onnistui. Tililläsi on nyt " + malli.getSaldo() + "€.");
+            
         }
-        else if (cardo.equals("A"))
-            totalvalue += 11;
-        else
-            totalvalue += Integer.parseInt(cardo);
-
+        else {
+            view.showMessage("Nostosi epäonnistui. Tililläsi on edelleen " + malli.getSaldo() + "€.");
+        }
+        // Näyttöä pyydetään siirtymään vain aloitusnäyttöön
+        view.startMenu();                        
+    }
+    */
+    
+    /*
+    public void talletus(double lisaRaha) {
+        view.talletus(lisaRaha);
+        // Ei kerrota nyt käyttäjälle toiminnon onnistumisesta mitään (???)
+        
+        // Näyttöä pyydetään siirtymään vain aloitusnäyttöön
+        view.startMenu();&
+    }
+    */
+    
+    public void play() {
+        // pelilogiikka tänne?
+        // startgame();
+        // askplayerhitorstand();
+        // ifHit jne....??
+        
+    }
+    
+            
+    // Koko sovelluksen käynnistäminen
+    public static void main(String[] args) {
+        // luodaan uusi kontrolleri-olio, joka valmistelee nyt sovelluksen
+        new Controller();
+        
     }
 }
+
