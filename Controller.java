67ce62eedc70137655ede6b38f1157d2eaa7ac3e
mkayyee <45204687+mkayyee@users.noreply.github.com>
2018-12-01 13:28:56 +0200

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -7,77 +7,25 @@ package blackjack;
 
 
 public class Controller extends Game {
-    // kapseloidut ominaisuudet (V, M)
-    private View view;
-    // private Hand hand;
-    
+    private final View view;
+    Game state;
     Game startgame;
     
     
     public Controller() {
-        // Nyt kontrollerin (C) konstruktori (eli tämä metodi) 
-        // luo muut tarvittavat komponentit ja pyytää sitten 
-        // näyttöä esittämään aloitusnäytön.
         
-        // luodaan sovelluksen tarvitsemat muut komponentit: M, V.
-        /* playerhand = new Hand();            // M, alkusaldo 500.
-        dealerhand = new Hand();
-        cardpack = new Hand();
-        */
         view = new View();                  // V
-        
-        // välitetään näytölle tieto sovelluksen kontrollerista eli
-        // tämä komponentti itse (this).
         view.setController(this); 
-        
         startgame = new Game();
-        // pyydetään näyttöä esittämään aloitusnäyttö
         view.startMenu();
         
         
                         
     }
     
-    /*
-    // näytöltä tulevia pyyntöjä
-    public double balance() {
-        double cardBalance = cardpack.getBalance();
-        return cardBalance;
-    }
-    */
-    
-    /*public void take(double card) {
-        boolean successcheck;
-        successcheck = cardpack.takecard(cards);
-        // Ei kerrota nyt käyttäjälle toiminnon onnistumisesta mitään (???)
-        if (successcheck)
-        { 
-            view.showMessage("Nostosi onnistui. Tililläsi on nyt " + malli.getSaldo() + "€.");
-            
-        }
-        else {
-            view.showMessage("Nostosi epäonnistui. Tililläsi on edelleen " + malli.getSaldo() + "€.");
-        }
-        // Näyttöä pyydetään siirtymään vain aloitusnäyttöön
-        view.startMenu();                        
-    }
-    */
-    
-    /*
-    public void talletus(double lisaRaha) {
-        view.talletus(lisaRaha);
-        // Ei kerrota nyt käyttäjälle toiminnon onnistumisesta mitään (???)
-        
-        // Näyttöä pyydetään siirtymään vain aloitusnäyttöön
-        view.startMenu();&
-    }
-    */
     
     public void play() {
-        // pelilogiikka tänne?
-        // startgame();
-        // askplayerhitorstand();
-        // ifHit jne....??
+        
         view.showMessage("Starting the game!");
         String vastaus = startgame.startGame(); 
         view.showQuestion(vastaus);
@@ -86,22 +34,22 @@ public class Controller extends Game {
     public void hit() { // pelaajalle annetaan yksi kortti lisää
         String hittered = startgame.Hit();
         view.showQuestion(hittered);
-        gamestate();
+        
     }
         
-    public void stand() { // pelaaja on lopettanut 
+    public void stand() { // jakajalle niin paljon kortteja että blackjack ehdot täyttyvät
         String stood = startgame.Stand();
         view.showQuestion(stood);
     }
     
     public void question(String message) {
-        view.showMessage(message);
+        view.showMessage(message); 
     }
 
     
-    // Koko sovelluksen käynnistäminen
+   
     public static void main(String[] args) {
-        // luodaan uusi kontrolleri-olio, joka valmistelee nyt sovelluksen
+        
         Controller controller = new Controller();
         
     }
