58ecac218bf2245e87a1a1fd176a152b55750838
mkayyee <45204687+mkayyee@users.noreply.github.com>
2018-12-02 03:10:55 +0200

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,11 +1,15 @@
 package blackjack;
 
+import Blackjack.Chips;
+
 
 public class Controller extends Game {
-    private final View view;
+    private View view;
     Game state;
     Game startgame;
-    
+    private static Chips chips = new Chips(1000);
+    int gain;
+    static int balanssi;
   
     public Controller() {
         
@@ -15,12 +19,30 @@ public class Controller extends Game {
         view.startMenu();
         
         
+        
+        
                         
     }
+    public static int chipAmount(){
+        balanssi = chips.getChips();
+        return balanssi;
+        
+    }
+    public int showChips(){
+        return chips.getChips();
+    }
+    
+    public static void bet(int amount){
+        chips.addChips(-amount);
+    }
+    
+    public static void win(int winnings){
+        chips.addChips(winnings);
+    }
+  
     
     
     public void play() {
-        
         view.showMessage("Starting the game!");
         String vastaus = startgame.startGame(); 
         view.showQuestion(vastaus);
@@ -45,7 +67,7 @@ public class Controller extends Game {
    
     public static void main(String[] args) {
         
-        Controller controller = new Controller();
+        new Controller();
         
     }
     
